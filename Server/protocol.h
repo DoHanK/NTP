6b300d812938a7292626e97e52d2bb640c18ac22
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2023-11-21 00:09:40 +0900

    패킷 사이즈 unsigned short 로 수정

--- Server/protocol.h
+++ Server/protocol.h
@@ -32,20 +32,20 @@ constexpr char SC_HITTED = 11;
 
 #pragma pack (push, 1)
 struct CS_LOGIN_PACKET {																															// C -> S 로그인 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	char			name[NAME_SIZE];
 };		
 
 																							 
 struct CS_CHANGE_COLOR_PACKET {																														// C -> S 색깔 바꾸기 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	int				color;
 };
 
 struct CS_MOVE_PACKET {																																// C -> S 이동 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	XMFLOAT3		pos;
 	XMFLOAT3		top_dir;
@@ -53,7 +53,7 @@ struct CS_MOVE_PACKET {																																// C -> S 이동 패킷
 };
 
 struct CS_BULLET_PACKET {																															// C -> S 총알 패킷
-	unsigned char							size;
+	unsigned short							size;
 	char									type;
 	std::array<XMFLOAT3, MAX_BULLETS>		bullets_pos;
 	std::array<XMFLOAT3, MAX_BULLETS>		bullets_dir;
@@ -61,26 +61,26 @@ struct CS_BULLET_PACKET {																															// C -> S 총알 패킷
 };
 
 struct CS_READY_PACKET {																															// C -> S 레디 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	bool			ready;
 };
 
 struct CS_ENTER_ROOM_PACKET {																														// C -> S 방 입장 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	int				color;
 };
 
 struct CS_ATTACK_PACKET {
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	int				id;
 	int				bullet_index;
 };
 
 struct SC_LOGIN_INFO_PACKET {																														// S -> C 로그인 정보 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	int				money;
@@ -91,14 +91,14 @@ struct SC_LOGIN_INFO_PACKET {																														// S -> C 로그인 정보
 };
 
 struct SC_CHANGE_COLOR_PACKET {																														// S -> C 색깔 바꾸기 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	int				color;
 };
 
 struct SC_ADD_PLAYER_PACKET {																														// S -> C 플레이어 추가 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	int				hp;
@@ -109,13 +109,13 @@ struct SC_ADD_PLAYER_PACKET {																														// S -> C 플레이어 추
 };
 
 struct SC_REMOVE_PLAYER_PACKET {																													// S -> C 플레이어 제거 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 };
 
 struct SC_MOVE_PLAYER_PACKET {																														// S -> C 이동 패킷
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	XMFLOAT3		pos;
@@ -124,14 +124,14 @@ struct SC_MOVE_PLAYER_PACKET {																														// S -> C 이동 패킷
 };
 
 struct SC_HITTED_PACKET {																															// S -> C 피격 패킷
-	unsigned char	size;	
+	unsigned short	size;	
 	char			type;
 	short			id;
 	int				hp;
 };
 
 struct SC_BULLET_PACKET {																															// S -> C 총알 패킷
-	unsigned char							size;
+	unsigned short							size;
 	char									type;
 	short									id;
 	std::array<XMFLOAT3, MAX_BULLETS>		bullets_pos;
@@ -140,14 +140,14 @@ struct SC_BULLET_PACKET {																															// S -> C 총알 패킷
 };
 
 struct SC_READY_PACKET {
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	bool			ready;
 };
 
 struct SC_ENTER_ROOM_PACKET {
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 	short			id;
 	char			name[NAME_SIZE];
@@ -156,7 +156,7 @@ struct SC_ENTER_ROOM_PACKET {
 };
 
 struct SC_GAME_START_PACKET {
-	unsigned char	size;
+	unsigned short	size;
 	char			type;
 };
 
